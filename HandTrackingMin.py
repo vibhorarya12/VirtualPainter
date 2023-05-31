cffbe7289bda7c06c39fcece970a70a3ce296878
myedu2502 <myedu2502@gmail.com>
2023-05-31 22:31:29 +0530

    Initial commit

---
+++ HandTrackingMin.py
@@ -0,0 +1,8 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(0)
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+print(width, height)
